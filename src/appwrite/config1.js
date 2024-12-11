95e86ef0db40818f57f5638e7a8de09a2f37de58
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-11 22:34:31 +0530

    get and create post

--- src/appwrite/config1.js
+++ src/appwrite/config1.js
@@ -1,2 +1,96 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable no-useless-catch */
 import config from "../config/config";
-import { Client, Account, ID } from "appwrite";
+import { Client,ID,Databases,Storage,Query } from "appwrite";
+
+export class Service{
+    client=new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setEndpoint(config.appwriteProjectId);
+        this.databases=new Databases(this.client)
+        this.bucket=new Storage(this.client)
+        
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,{
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,{
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            return error;
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
+        }
+    }
+}
+
+const service=new Service();
+export default service;
