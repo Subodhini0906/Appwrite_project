30691cb0474a29401dbf102f53b57c6836a00c6a
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-11 21:53:52 +0530

    create acc and login

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,3 +1,4 @@
+
 import config from "../config/config";
 import { Client, Account, ID } from "appwrite";
 
@@ -12,8 +13,30 @@ export class AuthService{
          this.account=new Account(this.client);
 
     }
+    async createAccount({email,password,name}){
+        try{
+            const userAccount=await this.account.create(ID.unique(),email,password,name);
+            if(userAccount){
+                return this.login({email,password});
+                
+            } else{
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        };
+        
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 const authService=new AuthService();
 
-export default AuthService
+export default authService
