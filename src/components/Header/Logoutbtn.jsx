f4a8e40e1ed3c13bbbff1a5d536d0b0bbbc29e22
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-14 20:37:19 +0530

    ..

---
+++ src/components/Header/Logoutbtn.jsx
@@ -0,0 +1,11 @@
+import { useDispatch } from "react-redux"
+import authService from "../../appwrite/config1"
+import {logout} from "../../store/authSlice"
+
+function Logoutbtn() {
+  return (
+    <div>Logoutbtn</div>
+  )
+}
+
+export default Logoutbtn
