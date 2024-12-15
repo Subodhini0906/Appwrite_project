eae74580e9ce5f215680b665b2302642c8d27541
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-15 20:22:26 +0530

    components added

---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+import React,{useId} from 'react'
+const Input=React.forwardRef(function Input({
+    label,
+    type='text',
+    className="",
+    ...props
+},ref){
+    return (
+        const id=useId()
+        <div className='w-full'>
+            {label && 
+            <label className='inline-block mb-1 pl-1' htmlFor={id}>
+                {label}
+            </label>
+            }
+        </div>
+    )
+} )
+
+export default Input
