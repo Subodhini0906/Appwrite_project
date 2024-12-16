f3c5d92b802eca2c17d4e471fd256502a9c5f07a
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-16 18:38:39 +0530

    post

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,7 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, RTE, Select } from "../Index";
+import appwriteService from "../../appwrite/config";
+import { data, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function PostForm() {
+function PostForm({post}) {
+    const {register,handleSubmit,watch,setValue,control,getValues}=useForm({
+        defaultValues:{
+            title:post?.title||"",
+            slug:post?.slug||"",
+            content:post?.content||"",
+            status:post?.status||"active"
+        }
+    })
+
+    const navigate=useNavigate();
+    const userData=useSelector(state=>state.user.userData)
+
+    const submit=async(data)=>{
+        if(post){
+            const file=data.image[0]?appwriteService.uploadFile(data.image[0]):null
+            if(file){
+                appwriteService.deleteFile(post.featuredImage)
+            }
+            const dbPost=await appwriteService.updatePost(post.$id,{...data,featuredImage: file?file.$id:undefined})
+        }
+    }
   return (
     <div>PostForm</div>
   )
